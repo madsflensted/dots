eecee02696c01f846612261a778a11b02fd6e5be
Mads Flensted-Urech <mfu@coadu.com>
2015-06-17 16:08:17 +0200

    remove comment code

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,12 +1,5 @@
 import {Socket} from "phoenix"
 
-// let socket = new Socket("/ws")
-// socket.connect()
-// let chan = socket.chan("topic:subtopic", {})
-// chan.join().receive("ok", chan => {
-//   console.log("Success!")
-// })
-
 function setup_elm() {
   let elmDiv = document.getElementById("elm-main");
   Elm.embed(Elm.Main, elmDiv)
